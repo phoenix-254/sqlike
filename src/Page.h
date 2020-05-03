9970dd19ecb715beda68387556bbcdfb3dc88ac0
shaishav <shaishav.shah.cs@gmail.com>
2020-05-03 19:50:32 -0400

    Added Page - Holds TwoWayList of records

---
+++ src/Page.h
@@ -0,0 +1,37 @@
+#ifndef SQLIKE_PAGE_H
+#define SQLIKE_PAGE_H
+
+#include "Record.h"
+#include "TwoWayList.cpp"
+
+class Page {
+private:
+    TwoWayList<Record> *records;
+
+    int numberOfRecs, currSizeInBytes;
+
+public:
+    Page();
+
+    ~Page();
+
+    // Takes a page and writes its binary representation to bits
+    void ToBinary(char *bits);
+
+    // Takes a binary representation of a page and gets the records from it
+    void FromBinary(char *bits);
+
+    // Returns first record from this page, returns 0 if there are no records.
+    // Note: It deletes the record returned.
+    int GetFirst(Record *record);
+
+    // Appends the new record to the end of the current page.
+    // Returns 1 if successfully appended, 0 otherwise.
+    // Note: The record is consumed, hence it won't have any value afterwards.
+    int Append(Record *record);
+
+    // Clear page contents.
+    void EmptyItOut();
+};
+
+#endif //SQLIKE_PAGE_H
