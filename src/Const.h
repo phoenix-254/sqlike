deeeec640899ad8d21f8b4debaf9e8207fc464e2
shaishav <shaishav.shah.cs@gmail.com>
2020-05-05 15:53:48 -0400

    main.cpp - final version

--- src/Const.h
+++ src/Const.h
@@ -6,19 +6,6 @@
 
 #define PAGE_SIZE 131072
 
-#define CATALOG_PATH "../../files/catalog"
-#define TBL_FILES_PATH "../../files/10mb/"
-#define BIN_FILES_PATH "../../bin/10mb/"
-
-#define TBL_REGION "region"
-#define TBL_NATION "nation"
-#define TBL_PART "part"
-#define TBL_SUPPLIER "supplier"
-#define TBL_PARTSUPP "partsupp"
-#define TBL_CUSTOMER "customer"
-#define TBL_ORDERS "orders"
-#define TBL_LINEITEM "lineitem"
-
 enum Target {
     Left,
     Right,
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
 
-#include "Const.h"
-#include "Record.h"
-#include "Schema.h"
+#include "Comparison.h"
+#include "ComparisonEngine.h"
+#include "Config.h"
 
 using namespace std;
 
+extern int yyparse();
+
+extern struct AndList *final;
+
 int main() {
-    Schema schema(CATALOG_PATH, TBL_NATION);
+    int tableIndex = 0;
+    while (tableIndex < 1 || tableIndex > 8) {
+        cout << ">>>> Select Table" << endl;
+        for (auto & t : TABLE_MAP) {
+            cout << "\t" << t.first << ". " << t.second << endl;
+        }
+
+        cin >> tableIndex;
+        if (tableIndex < 1 || tableIndex > 8)
+            cout << "Invalid choice, please try again!" << endl << endl;
+    }
+
+    const char *tableName = TABLE_MAP.find(tableIndex)->second;
+    cout << endl << ">>>> Printing Schema For " << tableName << endl;
+    Schema schema(CATALOG_FILE_PATH, tableName);
     schema.Print();
 
-    FILE *tblFile = fopen("../../files/10mb/nation.tbl", "r");
+    cout << endl << ">>>> Enter in your CNF and press (ctrl+d) for EOF: " << endl;
+    if (yyparse() != 0) {
+        cout << "ERROR : Invalid expression" << endl;
+        exit(1);
+    }
+
+    CNF cnf{};
+    Record literal;
+    cnf.GrowFromParseTree(final, schema, literal);
+    cout << endl << ">>>> CNF:" << endl;
+    cnf.Print();
+
+    char tblFilePath[100];
+    sprintf(tblFilePath, "%s%s.tbl", TBL_FILES_PATH, tableName);
+    FILE *tblFile = fopen(tblFilePath, "r");
 
     Record temp;
+    ComparisonEngine comparisonEngine;
+    cout << endl << ">>>> Printing Records" << endl;
     while (temp.SuckNextRecord(schema, tblFile)) {
-        temp.Print(schema);
+        if (comparisonEngine.Compare(&temp, &literal, &cnf)) {
+            temp.Print(schema);
+        }
     }
 
     return 0;
